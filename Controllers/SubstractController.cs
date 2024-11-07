94b27975438324663492050ee4a3885d9bdb0558
_Coder_Naren <narendrajalamkar8399@gmail.com>
2024-11-07 17:06:38 +0530

    Add project files.

---
+++ Controllers/SubstractController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5_SubstractionApplication.Controllers
+{
+    public class SubstractController : Controller
+    {
+        // GET: Substract
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/MinusModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5_SubstractionApplication.Models
+{
+    public class MinusModel
+    {
+        [Required(ErrorMessage="Enter Number1 to substract!!!")]
+        public int Number1 { get; set; }
+
+        [Required(ErrorMessage = "Enter Number2 to substract!!!")]
+        public int Number2 { get; set; }
+
+        public int? Answer { get; set; }
+    }
+}
